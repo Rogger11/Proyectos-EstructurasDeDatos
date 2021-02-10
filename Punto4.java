70cf0fc5eb42630dad62cc111872ea5bc639f804
unknown <emilton1103@gmail.com>
2021-02-10 16:41:42 -0500

    Correciones.

--- Punto4.java
+++ Punto4.java
@@ -4,12 +4,15 @@ public class Punto4 {
     public static void main(String[] args) {
         int n1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el primer número."));
         int n2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el número potencia."));
+        int con = n1;
         int resultado = 0;
-        int i = 0;
-        boolean listo = false;
-        while(i < n2){
-            resultado = resultado + (n1 + n1);
-            i++;
+        for (int i = 1; i<=n2; i++) {
+            int r = 0;
+            for(int j = 1; j <= con; j++){
+                r = r + n1;
+            }
+            resultado = n1;
+            n1 = r;
         }
         JOptionPane.showMessageDialog(null, "El resultado es: " + resultado);
     }
