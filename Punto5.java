cc3af0df0c8cec28e85a28de968dda18110f1940
unknown <emilton1103@gmail.com>
2021-02-10 17:07:35 -0500

    Segunda carga.

---
+++ Punto5.java
@@ -0,0 +1,21 @@
+package com.mycompany.repaso1;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+public class Punto5 {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<String>();
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el número a calcular el factorial."));
+        int Fact = 1;
+        String listaham = "1";
+        for (int i = 2; i <= numero; i++){
+            Fact = Fact * i;
+        }
+        for(int i = 1; i <= numero; i++){
+            lista.add("" + i);
+        }
+        for(int i = 1; i < numero; i++){
+            listaham += " x " + lista.get(i);
+        } 
+        JOptionPane.showMessageDialog(null, "El factorial de "+ numero +" = "+ numero +"! = "+ listaham +" = "+ Fact);
+    }
+}
